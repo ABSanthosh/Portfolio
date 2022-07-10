0f15b0edebc7ae0f50820219054cdf473831128f
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-11 00:57:53 +0530

    feat: Fonts added

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,16 @@
 <script>
   import { Router, Route } from "svelte-routing";
 
-  import Home from "./routes/Home.svelte";
+  // Pages
+  import Home from "./routes/Home/Home.svelte";
 
   export let url = "";
 </script>
+
+<svelte:head>
+  <style lang="scss" src="./styles/global.scss"></style>
+  <style lang="scss" src="./styles/fonts.scss"></style>
+</svelte:head>
 
 <Router {url}>
   <Route path="/"><Home /></Route>
