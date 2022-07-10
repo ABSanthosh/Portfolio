deb243984a67873ff51352f8f5f1e1eac18d93d1
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-11 00:56:38 +0530

    feat: Header component added

---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,18 @@
+<header class="HeaderWrapper">
+  <div class="HeaderWrapper__content">
+    <div class="HeaderWrapper__content--left">
+      <div class="HeaderWrapper__title">
+        <span class="HeaderWrapper__title--brackets">(</span>
+        <span class="HeaderWrapper__title--params">AB</span>
+        <span class="HeaderWrapper__title--brackets">)</span>
+        <span class="HeaderWrapper__title--arrow"> =></span>
+        <span class="HeaderWrapper__title--name">Santhosh</span>
+      </div>
+    </div>
+    <div class="HeaderWrapper__content--right">
+        <a href="https://github.com/ABSanthosh">Github</a>
+    </div>
+  </div>
+</header>
+
+<style lang="scss" src="./Header.scss"></style>
