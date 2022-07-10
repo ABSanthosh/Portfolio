06bd192686d9dda0f43b19e811dfb0580be4f303
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-11 00:57:38 +0530

    feat(Home): Basic structure done

---
+++ src/routes/Home/Home.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Header from "../../components/Header/Header.svelte";
+  const Santhosh="Santhosh";
+  const func = (AB)=>{Santhosh};
+
+  console.log(func())
+</script>
+
+<svelte:head>
+  <title>ABSanthosh</title>
+</svelte:head>
+
+<div class="HomeWrapper">
+  <Header />
+  <main class="HomeWrapper__main">
+    <section class="HomeWrapper__hero"><p>Coming Soon!</p></section>
+  </main>
+</div>
+
+<style lang="scss" src="./Home.scss"></style>
