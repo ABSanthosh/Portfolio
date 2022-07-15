ff375ac01824ad2ec5f355160a137a8b712770a0
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-16 01:44:26 +0530

    feat(component): Added experience item

---
+++ src/components/ExpItem/ExpItem.svelte
@@ -0,0 +1,14 @@
+<script>
+  let { position, company, desc, tStart, tEnd } = $$props;
+</script>
+
+<div class="ExpItemWrapper">
+  <h3 class="ExpItemWrapper--position">
+    {position}
+    <p class="ExpItemWrapper--range">{tStart} - {tEnd}</p>
+  </h3>
+  <h4 class="ExpItemWrapper--company">{company}</h4>
+  <p class="ExpItemWrapper--desc">{desc}</p>
+</div>
+
+<style lang="scss" src="./ExpItem.scss"></style>
