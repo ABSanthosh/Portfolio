f9d737480f85fd593a8b0880832201ee1cdec120
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-15 10:55:07 +0530

    comoponent: Fancy button/link

---
+++ src/components/FancyButton/FancyButton.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let { href, text, newTab, type = "link", action = () => {} } = $$props;
+</script>
+
+{#if type === "button"}
+  <button class="FancyButton FancyButton--button">
+    <slot />
+  </button>
+{:else if type === "link"}
+  <a
+    class="FancyButton FancyButton--link"
+    {href}
+    target={newTab ? "_blank" : ""}
+  >
+    <slot />
+  </a>
+{/if}
+
+<style lang="scss" src="./FancyButton.scss"></style>
