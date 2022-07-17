93a81faba007d3cd67b80b79530c8cf47886d123
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-18 01:57:04 +0530

    feat: colors for night and day theme

--- src/App.svelte
+++ src/App.svelte
@@ -16,3 +16,39 @@
 <Router {url}>
   <Route path="/"><Home /></Route>
 </Router>
+
+<style>
+  :global(body) {
+    --lightGray: #e7e5e4;
+    --lightPurple: #adacff;
+    --headerBackground: #fdfdfd3a;
+    --headerBrackets: #2532f5;
+    --headerParams: #e1944d;
+    --headerArrow: #9fd2c3;
+    --borderColor: #e6e6e6;
+    --headingColor: #2d3045;
+    --backgroundColor: #fdfdfd;
+    --baseTextColor: #3a3a3a;
+    --scrollbarThumb: #d3d1cb;
+    --scrollbarTrack: #edece9;
+    --textSelectionColor: #2daadb4d;
+
+    color-scheme: light;
+  }
+  :global(body.dark-mode) {
+    --lightGray: #5f5f5f;
+    --lightPurple: #2d3045;
+    --headerBackground: #2d3045;
+    --headerBrackets: #2532f5;
+    --headerParams: #e1944d;
+    --headerArrow: #0affa2;
+    --borderColor: #e6e6e6;
+    --headingColor: #ffffff;
+    --backgroundColor: #2d3045;
+    --baseTextColor: #fdfdfd;
+    --scrollbarThumb: #d3d1cb;
+    --scrollbarTrack: #edece9;
+    --textSelectionColor: #2daadb4d;
+    color-scheme: dark;
+  }
+</style>
