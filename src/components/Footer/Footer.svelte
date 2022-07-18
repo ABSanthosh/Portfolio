0948d4470f251ce7bc455ba6829ee19da7499527
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-19 03:25:39 +0530

    feat(Component): Footer

---
+++ src/components/Footer/Footer.svelte
@@ -0,0 +1,22 @@
+<script>
+  import SvelteIcon from "../../assets/Images/Languages/svelte.png";
+</script>
+
+<footer class="FooterWrapper">
+  <div class="FooterWrapper__container">
+    <div class="FooterWrapper__container--left">
+      Made with <span class="FooterWrapper__container--heart">&hearts;</span>
+      and
+      <a href="https://svelte.dev" target="_blank">
+        <img src={SvelteIcon} />
+      </a>
+      by
+      <a
+        href="https://www.linkedin.com/in/a-b-santhosh-97872612b/"
+        target="_blank">Santhosh</a
+      >
+    </div>
+  </div>
+</footer>
+
+<style lang="scss" src="./Footer.scss"></style>
