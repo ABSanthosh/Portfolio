9b39dfda2736535f344c815332c253e58344f3b4
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-26 01:30:58 +0530

    feat: Temp github card

---
+++ src/components/GithubMap/GithubMap.svelte
@@ -0,0 +1,25 @@
+<script>
+import { onMount } from "svelte";
+
+
+  // const data = fetch("https://gh-calendar.rschristian.dev/user/ABSanthosh", {
+  //   body: null,
+  //   method: "GET",
+  // })
+  //   .then((res) => {
+  //     return res.json();
+  //   })
+  //   .then((res) => {
+  //     console.log(res);
+  //   });
+
+</script>
+
+<div class="GithubMap">
+  <img
+    src="https://camo.githubusercontent.com/2414f9827ac4149fde1ae7b61b8480d6029d5a8fddf954d8449fa076f32712a1/68747470733a2f2f6769746875622d726561646d652d73747265616b2d73746174732e6865726f6b756170702e636f6d3f757365723d414253616e74686f736826646174655f666f726d61743d4d2532306a2535422532432532305925354426666972653d304544364444"
+    alt="Github Map"
+  />
+</div>
+
+<style lang="scss" src="./GithubMap.scss"></style>
