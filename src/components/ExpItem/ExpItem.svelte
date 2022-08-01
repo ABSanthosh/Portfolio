a800070c67627e7f83c3a1835a61c4ccc0efb776
ABSanthosh <a.b.santhosh02@gmail.com>
2022-08-02 02:47:58 +0530

    chore: Responsive Exp item

--- src/components/ExpItem/ExpItem.svelte
+++ src/components/ExpItem/ExpItem.svelte
@@ -5,9 +5,11 @@
 <div class="ExpItemWrapper">
   <h3 class="ExpItemWrapper--position">
     {position}
-    <p class="ExpItemWrapper--range">{tStart} - {tEnd}</p>
   </h3>
-  <h4 class="ExpItemWrapper--company">{company}</h4>
+  <h4 class="ExpItemWrapper--company">
+    {company}
+    <p class="ExpItemWrapper--range">{tStart} - {tEnd}</p>
+  </h4>
   <p class="ExpItemWrapper--desc">{desc}</p>
 </div>
 
