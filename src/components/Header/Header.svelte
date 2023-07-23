0b51403e799d157a7644716b14424b173b26c66d
ABSanthosh <a.b.santhosh02@gmail.com>
2023-07-23 19:24:30 +0530

    feat: theme switcher

--- src/components/Header/Header.svelte
+++ src/components/Header/Header.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-  import "./Header.scss";
+  import { theme } from "$lib/stores";
+
+  // TODO: Add onMount to gracefully switch default theme
+  // TODO: Get resume link from global store
 </script>
 
 <header class="HeaderWrapper">
@@ -18,7 +21,31 @@
         href="https://raw.githubusercontent.com/ABSanthosh/Portfolio/main/public/assets/Docs/AB%20Santhosh%20Resume.pdf"
         target="_blank">Resume</a
       >
-      <button class="FancyButton">Dark</button>
+      <button
+        class="FancyButton"
+        on:click={() => {
+          theme.set($theme === "dark" ? "light" : "dark");
+        }}
+      >
+        <svg viewBox="0 0 20 20" width="16px" height="16px" fill="currentColor">
+          {#if $theme === "dark"}
+            <path
+              d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"
+            />
+          {:else}
+            <path
+              fill-rule="evenodd"
+              d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+              clip-rule="evenodd"
+            />
+          {/if}
+        </svg>
+        {$theme}
+      </button>
     </div>
   </div>
 </header>
+
+<style lang="scss">
+  @import "./Header.scss";
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,27 @@
+<script lang="ts">
+  import { theme } from "$lib/stores";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    theme.subscribe((value) => {
+      if (document) {
+        document.documentElement.setAttribute("data-theme", value);
+      }
+    });
+  });
+</script>
+
 <svelte:head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <meta name="keywords" content="ABSanthosh" />
   <meta name="author" content="ABSanthosh" />
+
+  <meta name="color-scheme" content={$theme} />
+
+  <link rel="stylesheet" href={`/theme/light.css`} />
+  <link rel="stylesheet" href={`/theme/dark.css`} />
+  <link rel="stylesheet" href={`/theme/system.css`} />
 
   <title>ABSanthosh</title>
 </svelte:head>
