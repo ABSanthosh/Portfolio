44248d852f14fed509a1d961446e47ad38767e11
ABSanthosh <a.b.santhosh02@gmail.com>
2024-08-26 02:21:07 -0400

    refactor: Add new page for loading blog posts

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import "@/styles/root/markdown.scss";
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<svelte:head>
+  <link rel="stylesheet" href="/theme/prism.css" />
+</svelte:head>
+
+<main class="Article Page">
+  <article class="MarkdownBody">
+    {@html data.html}
+  </article>
+</main>
+
+<style lang="scss" src="../../../styles/routes/article.scss" global></style>
